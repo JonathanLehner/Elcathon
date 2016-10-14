53276ff0d9f394d68b1a82fe7d1bf7ac1ebe9eaf
Min Zhao <sebzhaomin@gmail.com>
2016-10-15 00:39:48 +0200

    Work on shopping list adding

---
+++ Client/MobileApp/MobileApp/MobileApp/ViewModels/AddShoppingItemPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MobileApp.ViewModels
+{
+    public class AddShoppingItemPageViewModel : BindableBase, INavigationAware
+    {
+        public AddShoppingItemPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        }
+    }
+}
---
+++ Client/MobileApp/MobileApp/MobileApp/Views/AddShoppingItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MobileApp.Views
+{
+    public partial class AddShoppingItemPage : ContentPage
+    {
+        public AddShoppingItemPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            const double STEPVALUE = 1.0;
+            var newStep = Math.Round(e.NewValue / STEPVALUE);
+
+            Slider.Value = newStep * STEPVALUE;
+        }
+    }
+}
